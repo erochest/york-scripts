84df1b538bfb30fe7f6817fda4e4d938a7d75ac1
Eric Rochester <erochest@virginia.edu>
2015-05-20 16:43:10 -0400

    Created a script to do a little data munging.
    * .gitignore

--- Makefile
+++ Makefile
@@ -7,12 +7,17 @@ YorkScripts: cabal.sandbox.config YorkScripts.hs
 init: cabal.sandbox.config deps
 
 deps:
+	cabal install cassava text text-format conduit conduit-combinators conduit-extra cassava-conduit bytestring
+
+data/bill-id.csv: YorkScripts data/chisqr.csv
+	./YorkScripts < data/chisqr.csv > data/bill-id.csv
 
 cabal.sandbox.config:
 	cabal sandbox init
 
 clean:
 	rm -f *.o *.hi *.html
+	rm -f data/bill-id.csv
 
 distclean: clean
 	rm -f YorkScripts
--- YorkScripts.hs
+++ YorkScripts.hs
@@ -4,5 +4,60 @@
 module Main where
 
 
+import           Conduit
+import           Control.Exception
+import           Data.Csv
+import           Data.Csv.Conduit
+import qualified Data.Text         as T
+import           Data.Text.Format
+import           Data.Text.Lazy    (toStrict)
+
+
+-- key id
+-- bill id
+-- bill id 2
+-- cong
+-- bill type
+-- bill no
+-- name full
+-- description
+-- intr date
+-- intr month
+-- year
+-- major topic
+-- sub topic code
+-- cap major topic
+-- cap sub topic
+-- plaw
+-- plaw date
+-- plaw no
+-- pass h
+-- pass s
+-- majority
+-- party
+-- chamber
+-- commem
+-- congress
+
+line :: [T.Text] -> [T.Text]
+line header@("KeyID":_)   = header
+line (keyId:billId:_:row) = keyId : billId : field billId : row
+line row                  = row
+
+field :: T.Text -> T.Text
+field = toStrict . format "{}{} ({})" . parseBillId . T.split (== '-')
+
+parseBillId :: [T.Text] -> (T.Text, T.Text, T.Text)
+parseBillId (session:house:bill:_) = (T.take 1 house, bill, session)
+parseBillId _ = ("ERROR", "", "")
+
 main :: IO ()
-main = undefined
+main = runResourceT $  stdinC
+                    $= fromCsvLiftError liftError defaultDecodeOptions NoHeader
+                    $= mapC line
+                    $$ toCsv defaultEncodeOptions
+                    =$ stdoutC
+
+liftError :: CsvParseError -> IOException
+liftError (CsvParseError _ msg)  = undefined
+liftError (IncrementalError msg) = undefined
